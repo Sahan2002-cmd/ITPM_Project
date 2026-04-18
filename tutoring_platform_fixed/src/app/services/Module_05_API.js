32fa00ace42c38243b4ade90ce8ed8b6a42e70ed
Sahan2002-cmd <it23837676@.my.sliit.lk>
2026-04-18 22:38:54 +0530

    update and change mongo URL

--- tutoring_platform_fixed/src/app/services/Module_05_API.js
+++ tutoring_platform_fixed/src/app/services/Module_05_API.js
@@ -25,7 +25,7 @@
  *   - Student is notified before auto-deletion
  */
 
-const BASE_URL = "https://localhost:44331/api";
+const BASE_URL = "http://localhost:4000";
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
